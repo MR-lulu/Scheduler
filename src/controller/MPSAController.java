d1b06d5b3e1f0395ed4f63d41c91a2b372f95225
1024909687@qq.com <1024909687@qq.com>
2017-07-07 10:58:17 +0800

    完成

--- src/controller/MPSAController.java
+++ src/controller/MPSAController.java
@@ -5,7 +5,11 @@ import java.util.LinkedList;
 
 import algorithm.FactoryMPSA;
 import algorithm.FactoryPSA;
+import algorithm.MPSA;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import model.PCB;
 import model.ResultModel;
@@ -18,6 +22,8 @@ import model.ResultModel;
 public class MPSAController extends Controller{
 	@FXML private TableColumn<ResultModel, Integer> cpuColumn;
 	@FXML private TableColumn<ResultModel, Integer> priorityColumn;
+	@FXML private ComboBox<Integer> choose;
+	@FXML private Label cpunum;
 	
 	@FXML private void initialize() {
 		setUpTableView();
@@ -26,6 +32,12 @@ public class MPSAController extends Controller{
 		priorityColumn.setCellValueFactory(cellData->cellData.getValue().priorityProperty().asObject());
 		priorityColumn.setCellFactory(new Cell());
 		scheduler = new FactoryMPSA().cteate();
+		choose.setItems(FXCollections.observableArrayList(1,2,3,4,5,6,7,8));
+		choose.getSelectionModel().selectedItemProperty().addListener((ov,oldV,newV)->{
+			MPSA mpsa = (MPSA)scheduler;
+			mpsa.setCpumun(newV.intValue());
+			num.setText(String.valueOf(newV.intValue()));
+		});
 	}
 
 	@Override

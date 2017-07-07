eba7b2244469359b62d523330b5c592e5a943f75
Administrator <Administrator@MS-20160925BGKJ>
2017-07-07 09:05:31 +0800

    时间片和先来先服务测试

---
+++ test/algorithm/FCFSTest.java
@@ -0,0 +1,38 @@
+package algorithm;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import misc.InitializationFormFile;
+import model.PCB;
+import model.ResultModel;
+
+public class FCFSTest {
+
+	@Test
+	public void test() {
+		String [] myList = {"1000","1000","1000","1000","1000","1000","1000","1001","1002","1002","1002","1003","1003","1003","1003","1003","1004","1004","1004","1004"}; // 打印所有数组元素 
+		boolean retVal;
+		Factory factory = new FactoryFCFS();
+		Scheduler scheduler = factory.cteate();
+		LinkedList<PCB> linkedList = InitializationFormFile.getLinkedListFormFile();
+		ObservableList<ResultModel> resultData = FXCollections.observableArrayList();
+		for(PCB pcb:linkedList){
+			resultData.add(new ResultModel(pcb,pcb.getPid(), pcb.getState(), pcb.getPriority(), 
+					0, pcb.getArriveTime(),pcb.getNeedTime(), 0, 0, 0,0));
+		}
+		scheduler.dynamicRun(resultData);
+		String[] str2 = scheduler.getList().toArray(new String[myList.length]);
+		assertArrayEquals(myList,str2);
+		for (int i = 0 ; i < myList.length ; i++) {
+			System.out.println(myList[i]); 
+			} 
+		} 
+	}
+
+
